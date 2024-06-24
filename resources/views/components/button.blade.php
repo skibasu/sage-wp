6e00dc90fe9a27b5e46c7040f1a449a50807122e
su <lukasz.skiba@frontdoor.com>
2024-06-24 16:01:13 +0200

    HomePageBlocks:comment-added

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -1,3 +1,4 @@
+{{-- Type needed : buttton / link (<a href></a>) --}}
 <button
     {{ $attributes->merge([
         'class' => "rounded-full inline-flex items-center justify-items-center text-center font-bold px-[12px] py-[2px] text-button-d h-[30px] lg:px-[24px] lg:py-[4px] lg:text-button-d lg:h-[38px] disabled:cursor-not-allowed {$variant_class}",
